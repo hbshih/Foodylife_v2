a3f1c03f56a5e0c8e00bf972918701db95751993
“hbshih” <hbshih@gmail.com>
2017-12-12 14:50:58 +0000

    Fix Firebase bug

--- Food Diary App!/AppDelegate.swift
+++ Food Diary App!/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        //Firebase
-        FIRApp.configure()
+        //Firebases
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
