09596013cf480addfc8b8d60353ee48bb476134d
“hbshih” <hbshih@gmail.com>
2017-12-14 21:35:35 +0000

    Homepage Screen -> Color adjustment fixed

--- Food Diary App!/homepageViewController.swift
+++ Food Diary App!/homepageViewController.swift
@@ -25,29 +25,39 @@ class homepageViewController: UIViewController {
                 let newAngleView = newAngle()
                 print(newAngleView)
                 circularSlider.animate(toAngle: newAngleView, duration: 0.5, completion: nil)
-                
-        
     }
     
     func newAngle() -> Double
     {
-        if circularSlider.angle == 360
+        print(circularSlider.angle)
+        if circularSlider.angle >= 360
         {
-            circularSlider.angle = 0
+            print("----- Start Again -----")
+            return 0
+        }
+        // Red -- Bad Health
+        if circularSlider.angle >= 0 && circularSlider.angle <= 30
+        {
+            circularSlider.set(colors: UIColor(red:0.99, green:0.44, blue:0.39, alpha:1.0))
+            circularSlider.trackColor = UIColor(red:0.99, green:0.44, blue:0.39, alpha:0.2)
+        }// Yellow -- Neutral
+        else if circularSlider.angle > 30 && circularSlider.angle <= 180
+        {
+            circularSlider.set(colors: UIColor(red:0.99, green:0.82, blue:0.39, alpha:1.0))
+            circularSlider.trackColor = UIColor(red:0.99, green:0.82, blue:0.39, alpha:0.2)
+        }
+            // Blue -- Good
+        else if circularSlider.angle > 180 && circularSlider.angle <= 330
+        {
+            circularSlider.set(colors:UIColor(red:0.39, green:0.82, blue:0.99, alpha:1.0))
+            circularSlider.trackColor = UIColor(red:0.39, green:0.82, blue:0.99, alpha:0.2)
+        }else if circularSlider.angle > 330 && circularSlider.angle < 360
+        {
+            circularSlider.set(colors: UIColor(red:0.60, green:0.80, blue:0.29, alpha:1.0))
+            circularSlider.trackColor = UIColor(red:0.60, green:0.80, blue:0.29, alpha:0.2)
         }
         
-        if circularSlider.angle > 90
-        {
-            /*
-            let colorBlue = UIColor(red: 134, green: 214, blue: 247, alpha: 1)
-            let colorBlueHue = UIColor(red: 211, green: 242, blue: 255, alpha: 1)
-            circularSlider.set(colors: color.blue)
-            circularSlider.trackColor = colorBlueHue
- */
-        }
-        
-        var currentAngle = circularSlider.angle
-        var newAngle = currentAngle + 10
-        return newAngle
+        let currentAngle = circularSlider.angle
+        return currentAngle + 10
     }
 }
